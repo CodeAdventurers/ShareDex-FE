42e9f96c6fb3d11b8f4e32419078b03be6ee04e7
zhengjunbin <zhengjunbin@fuzhi.ai>
2023-06-03 01:13:04 +0800

    chore: 配置cz

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
   return (
     <>
       <div className="App">万里长城第一步</div>
-      <div className="bg-red-400 w-24">万里长城第d步</div>
+      <div className="bg-red-400 w-24">万里长城第2步</div>
     </>
   );
 }
