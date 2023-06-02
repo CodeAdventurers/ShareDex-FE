b7b998994ba756fa29ff692ccdceb967f8a12fbd
zhengjunbin <zhengjunbin@fuzhi.ai>
2023-06-03 00:42:59 +0800

    feat: 项目搭建

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <div className="App">万里长城第一步</div>
+      <div className="bg-red-400 w-24">万里长城第二步</div>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, normalizePath } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import viteEslint from 'vite-plugin-eslint';
+
+// 全局 scss 文件的路径 用 normalizePath 解决 window 下的路径问题
+const variablePath = normalizePath(path.resolve('./src/variable.scss'));
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), reactRefresh(), viteEslint()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  // css 相关的配置
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // additionalData 的内容会在每个 scss 文件的开头自动注入
+        additionalData: `@import "${variablePath}";`
+      }
+    },
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')]
+    }
+  }
+});
