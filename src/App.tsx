c74e055cb72fe351f7a53306ea056c6cac9fa836
abean <abeanz@163.com>
2023-06-03 12:56:39 +0800

    feat: 项目初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import './App.scss';
 
 function App(): JSX.Element {
   return (
     <>
       <div className="App">万里长城第一步</div>
-      <div className="bg-red-400 w-24">万里长城第2步</div>
+      <div className="bg-red-400 w-24">万里长城第2 步</div>
     </>
   );
 }
