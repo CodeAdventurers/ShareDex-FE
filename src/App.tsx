35ab855a9563b541292fcafcba9cc9f39c8951d5
abean <abeanz@163.com>
2023-06-04 22:06:42 +0800

    feat: 安装antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss';
+import { Button } from 'antd';
 
 function App(): JSX.Element {
   return (
     <>
       <div className="App">万里长城第一步</div>
       <div className="bg-red-400 w-24">万里长城第2 步</div>
+      <Button>Default Button</Button>
     </>
   );
 }
